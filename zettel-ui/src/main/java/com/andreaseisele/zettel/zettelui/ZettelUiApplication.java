a7c7521cdd169f820a6ea9daab6981ad44083c50
Andreas Eisele <ae@andreaseisele.com>
2020-09-01 14:09:10 +0200

    initial scaffold

---
+++ zettel-ui/src/main/java/com/andreaseisele/zettel/zettelui/ZettelUiApplication.java
@@ -0,0 +1,13 @@
+package com.andreaseisele.zettel.zettelui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZettelUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZettelUiApplication.class, args);
+	}
+
+}
---
+++ zettel-ui/src/test/java/com/andreaseisele/zettel/zettelui/ZettelUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreaseisele.zettel.zettelui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZettelUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
